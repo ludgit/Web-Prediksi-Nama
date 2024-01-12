03fbe85caf8a6a7c1f653e0d70a4cdd6d007c261
LudGithub <149161520+ludgit@users.noreply.github.com>
2024-01-12 22:50:57 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
-console.log("Halo");
-
 const base_api = "https://genderapi.io/api";
 const api = "659a7e7fcbee3b71df05dc23";
 
 function showResult(name, gender, probability) {
     const predictionElement = document.getElementById("prediction");
+    let genderMale = document.getElementById("male")
+    let genderFemale = document.getElementById("female")
     let probabilityPercentage = probability;
     let ubahGender;
 
+    genderMale.style.display = 'none';
+    genderFemale.style.display = 'none';
+    
     if (gender == "male") {
         ubahGender = "Laki-Laki";
+        genderMale.style.display = 'inline'
+        genderFemale.style.display = 'none'
     } else {
         ubahGender = "Perempuan";
+        genderMale.style.display = 'none'
+        genderFemale.style.display = 'inline'
     }
 
 
@@ -20,7 +27,7 @@ if (probabilityPercentage == 0) {
         predictionElement.innerHTML = predictionText;
     } 
     else {
-        let predictionText = `Halo <strong style="background-color: aquamarine">${name}</strong>, Jenis Kelamin Kamu Kemungkinan Adalah <strong style="background-color: aquamarine">${ubahGender}</strong> Dengan Presentase Sebesar <strong style="background-color: aquamarine">${probabilityPercentage}%</strong>`;
+        let predictionText = `Halo <strong class="warnajawab">${name}</strong>, Jenis Kelamin Kamu Kemungkinan Adalah <strong class="warnajawab">${ubahGender}</strong> Dengan Presentase Sebesar <strong class="warnajawab">${probabilityPercentage}%</strong>`;
         predictionElement.innerHTML = predictionText;
     }
 }
